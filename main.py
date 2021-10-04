d4afee27aa37da3d67c21b759f787e230f70e46f
vphnguyen <48557263+vphnguyen@users.noreply.github.com>
2021-10-04 14:14:36 +0700

    with db

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import hashlib
 
-# File to check
+#==========================    INIT
 salt="salty_sonny"
 file_name = 'noidung.txt'
-# Correct original md5 goes here
+#==========================    MD5 Filename
 original_md5 = '5d41402abc4b2a76b9719d911017c592'  
 hashed_filename = hashlib.md5(file_name.encode()).hexdigest()
 print("Hashed file name: "+hashed_filename)
 
 
-# Open,close, read file and calculate MD5 on its contents 
+#==========================    MD5 File content
 with open(file_name , "rb") as file_to_check:
-    # read contents of the file
+    #==========================    get byte
     data = file_to_check.read()    
-    # pipe contents of the file through
-    md5_returned = hashlib.md5(data+(hashed_filename+salt).encode()).hexdigest()
-    print(file_name +" MD5: "+ md5_returned)
-# Finally compare original MD5 with freshly calculated
-if original_md5 == md5_returned:
-    print ("MD5 verified.")
-else:
-    print ("MD5 verification failed!.")
+    #==========================    MD5 with salt and md5 file name
+    md5_overall = hashlib.md5(data+(hashed_filename+salt).encode()).hexdigest()
+    print(file_name +" MD5: "+ md5_overall)
+
+
+infile =open ('md5_db.txt','r')
+for line in infile:
+    print("Checking with: "+ line)
+    if md5_overall == line.strip() :
+        print ("MD5 ton tai trong DB.")
+    else:
+        print ("Khong tim thay MD5 nao.")
+infile.close()
+
+if input("Wanna write to db ? ")=="yes":
+    outfile =open ('md5_db.txt','w')
+    print(md5_overall,file=outfile)
+    outfile.close()
